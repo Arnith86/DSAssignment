633b4ad34911a391454dd256533bb5d2a9477ff7
a21jeaha <a21jeaha@student.his.se>
2023-09-07 11:52:15 +0200

    added a rudimentary heartbeat class

--- src/main/java/javaClient/JavaClient.java
+++ src/main/java/javaClient/JavaClient.java
@@ -8,58 +8,51 @@ import java.util.concurrent.TimeUnit;
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
-import org.zeromq.ZMQ.Socket;
+// import org.zeromq.ZMQ.Socket;
 
 public class JavaClient implements Runnable {
 	
 	ScheduledExecutorService heartbeatTimer;
 	
 	public JavaClient (){
+		// IN TESTING 
 		heartbeatTimer = Executors.newScheduledThreadPool(1);
+		//heartbeatTimer.execute(null);
+		
+		
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello world");
-		JavaClient javaClient = new JavaClient();
-//		
-//		Thread javaClientThread = new Thread(javaClient);
-//		javaClientThread.start(); 
-		javaClient.run();
-		
-		//		javaClient.enterQueue();
-		//		javaClient.getCurrentQueue();
-	}
+
 	
 
 	@Override
 	public void run() {
-		enterQueue();
-		getCurrentQueue();
-		heartbeatTimer.scheduleWithFixedDelay(heartbeat(), 500, 500, TimeUnit.MILLISECONDS);
+//		enterQueue();
+//		getCurrentQueue();
+		//heartbeatTimer.scheduleWithFixedDelay(heartbeat(), 500, 500, TimeUnit.MILLISECONDS);
 		
 		
 	}
 	
-	private Runnable heartbeat() {
-		
-		String heartBeat = "{\r\n"
-				+ "    \"name\": \"JP\",\r\n"
-				+ "    \"clientId\": \"JP\"\r\n"
-				+ "}"; 
-		
-		try(ZContext context = new ZContext()){
-			
-			ZMQ.Socket socket = context.createSocket(SocketType.REQ); 
-			socket.connect("tcp://ds.iit.his.se:5556");
-			socket.send(heartBeat.getBytes(ZMQ.CHARSET),0);
-			
-		}
-		return null; 
-	}
+//	private Runnable heartbeat() {
+//		
+//		String heartBeat = "{\r\n"
+//				+ "    \"name\": \"JP\",\r\n"
+//				+ "    \"clientId\": \"JP\"\r\n"
+//				+ "}"; 
+//		
+//		try(ZContext context = new ZContext()){
+//			
+//			ZMQ.Socket socket = context.createSocket(SocketType.REQ); 
+//			socket.connect("tcp://ds.iit.his.se:5556");
+//			socket.send(heartBeat.getBytes(ZMQ.CHARSET),0);
+//			
+//		}
+//		return null; 
+//	}
 	
 	private void getCurrentQueue() {
-		
+		// SEEMS TO WORK FINE 
 		try(ZContext context = new ZContext()){
 			ZMQ.Socket subscriber = context.createSocket(SocketType.SUB);
 			subscriber.connect("tcp://ds.iit.his.se:5555");
@@ -76,24 +69,42 @@ public class JavaClient implements Runnable {
 	}
 	
 	private void enterQueue() {
-		
-		String enterQueue = "{\r\n"
-				+ "    \"enterQueue\": true,\r\n"
-				+ "    \"name\": \"JP\",\r\n"
-				+ "    \"clientId\": \"JP\"\r\n"
-				+ "}";
-		
+		// NOT WORKING CORRECTLY 
 		try(ZContext context = new ZContext()){
 			
 			ZMQ.Socket socket = context.createSocket(SocketType.REQ); 
 			socket.connect("tcp://ds.iit.his.se:5556");
 			
 			System.out.println("Placing in Queue");
+			
+			String enterQueue = "{\"enterQueue\": true, \"name\": \"JP\", \"clientId\": \"JP\"}";
+			
 			socket.send(enterQueue.getBytes(ZMQ.CHARSET),0);
 			System.out.println("Placed in queue");
 			
+			byte[] reply = socket.recv(0); 
+			
+			System.out.println("this was recived: " + new String(reply, ZMQ.CHARSET));
+			
+			// IN TESTING 
+			JavaClientHeartbeatTread heartbeat = new JavaClientHeartbeatTread();
+			Thread heartbeatThread = new Thread(heartbeat);
+			heartbeatThread.start(); 
+			// IN TESTING 
 		}
 		
 	}
-	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("hello world");
+		JavaClient javaClient = new JavaClient();
+		
+		javaClient.enterQueue();
+		javaClient.getCurrentQueue();
+		
+		while(true) {}
+		
+	}
 }
---
+++ src/main/java/javaClient/JavaClientHeartbeatTread.java
@@ -0,0 +1,60 @@
+package javaClient;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+public class JavaClientHeartbeatTread implements Runnable {
+	
+	public JavaClientHeartbeatTread (){
+		
+	}
+	
+	
+	@Override
+	public void run() {
+//		  ScheduledExecutorService makingACoffee;
+//        makingACoffee = Executors.newScheduledThreadPool(1);
+//        makingACoffee.scheduleWithFixedDelay(heartbeat(), 2000, 2000, TimeUnit.MILLISECONDS);//make coffee every 2 seconds
+		while(true) {
+			heartbeat();
+		}
+	}
+	
+	private void heartbeat() {
+		
+		String heartBeat = "{\r\n"
+				+ "    \"name\": \"JP\",\r\n"
+				+ "    \"clientId\": \"10\"\r\n"
+				+ "}"; 
+		
+		try(ZContext context = new ZContext()){
+			
+			ZMQ.Socket socket = context.createSocket(SocketType.REQ); 
+			socket.connect("tcp://ds.iit.his.se:5556");
+			socket.send(heartBeat.getBytes(ZMQ.CHARSET),0);
+			byte[] reply = socket.recv(0); 
+			
+			ZMQ.Socket error = context.createSocket(SocketType.SUB);
+			error.connect("tcp://ds.iit.his.se:5555");
+			
+			error.subscribe("queue");
+			
+			String errorMessage =  new String(error.recv(), ZMQ.CHARSET);
+			String msg =  new String(error.recv(), ZMQ.CHARSET);
+			
+			System.out.println(errorMessage);
+			System.out.println(msg);
+		
+		}
+		
+		
+		System.out.println("Hello FROM heartbeat!");
+		// return null; 
+	}
+
+}
