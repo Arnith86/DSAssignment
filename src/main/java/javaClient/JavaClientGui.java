524fa8e5da122471ba2c1e637fdcdac102c6f05c
a21jeaha <a21jeaha@student.his.se>
2023-09-10 11:57:58 +0200

    added a top panel. with related functions
    Label with information for the user
    text input
    send button

--- src/main/java/javaClient/JavaClientGui.java
+++ src/main/java/javaClient/JavaClientGui.java
@@ -3,24 +3,57 @@ package javaClient;
 import java.awt.BorderLayout;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class JavaClientGui {
+	// used for test on  the GUI, should be removed when the implementation is finished
+	private String[] testArray = {"JP", "ADAM", "MARK", "SVEN", "MIST"};  
+	
+	
+	private JLabel instructionText; 
+	
+	
 	private JFrame applicationFrame;
-	// the name should be changed to somthing more apropriet
-	private JPanel panel;
+	
+	private JPanel inputPanel;
+	private JPanel textInputPanel; 
+	private JTextField nameInput; 
+	private JButton sendButton; 
+	
+	
 	private JPanel queuePanel; 
 	
 	
-	private JLabel testLabel = new JLabel("TEst TExt"); 
+	
 	public JavaClientGui () {
+		
 		applicationFrame = new JFrame("Queue");
-		panel = new JPanel(); 
+		
+			
+		inputPanel = new JPanel(); 
+		inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
+		instructionText = new JLabel("Supply your name and press the send button!");
+		textInputPanel = new JPanel();
+		nameInput = new JTextField("Name");
+		sendButton = new JButton("Send");
+		textInputPanel.add(nameInput);
+		textInputPanel.add(sendButton);
+		inputPanel.add(instructionText);
+		inputPanel.add(textInputPanel);
+		
+		
+		
+		
 		// not used yet
 		// 	panel.setLayout(new BorderLayout(, BoxLayout.Y_AXIS));
 		
+		// Frame setup
+		applicationFrame.add(inputPanel);
+		// Some more configuration might be needed 
 		applicationFrame.setSize(800, 800);
 		applicationFrame.setVisible(true);
 		applicationFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
