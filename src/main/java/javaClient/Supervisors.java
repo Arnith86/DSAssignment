037a7100f480b7742a13ca96b4778289ab1c4a48
a21jeaha <a21jeaha@student.his.se>
2023-09-13 19:43:44 +0200

    added more values to Supervisors
    and expandet the setter

--- src/main/java/javaClient/Supervisors.java
+++ src/main/java/javaClient/Supervisors.java
@@ -2,7 +2,21 @@ package javaClient;
 
 public class Supervisors {
 	
-	public Supervisors() {
-		// WE ARE NOT reciving any values yet 
+	private String name; 
+	private String status; 
+	
+	private String studentName;
+	private int studentTickit; 
+	
+	public Supervisors(String name, String status) {
+		// WE ARE NOT reciving any values yet
+		this.name = name;
+		this.status = status;
+		studentName = null; 
+	}
+	
+	public void setSupervising(String supervising, int tickit) {
+		this.studentName = supervising;
+		this.studentTickit = tickit; 
 	}
 }
