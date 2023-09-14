35ee9e0da46b43324e619bce13bd035749af34ee
c21adano <102599332+c21adano@users.noreply.github.com>
2023-09-14 15:53:20 +0200

    added dictionary to json convertion
    added dictionary to json convertion and publisher socket for the server client.

--- C#/DSAssignmentC#/ConsoleApp1/QueueServer.cs
+++ C#/DSAssignmentC#/ConsoleApp1/QueueServer.cs
@@ -1,5 +1,46 @@
+using System.Xml;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using NetMQ.Sockets;
+using NetMQ;
+
 namespace QueueServerNameSpace{
     static partial class QueueServer{
-        public static void 
+        public static void List()
+        {
+            //Dictionary that hold the queue list
+            Dictionary<int, string> queueList = new Dictionary<int, string>();
+
+            //add three diffrent element to the dictionary, dictionary.Add(x, x) to add new elements.
+            queueList.Add(1, "One");
+            queueList.Add(2, "Two");
+            queueList.Add(3, "Three");
+
+            static string MyDictionaryToJson(IDictionary<int, string> dict)
+            {
+                var x = dict.Select(d =>
+                    string.Format("\"ticket\": {0}, \"name\": \"{1}\"", d.Key, string.Join(",", d.Value)));
+                return "[{" + string.Join("},{", x) + "}]";
+            }
+            string js2 = MyDictionaryToJson(queueList);
+
+            Console.WriteLine(js2);
+
+            while (true)
+            {
+                using (var pub = new PublisherSocket())
+                {
+                    pub.Bind("tcp://*:5555");
+
+
+                    while (true)
+                    {
+                        pub.SendMoreFrame("queue").SendFrame(js2); // Message
+
+
+                    }
+                }
+            }
+        }
     }
 }
