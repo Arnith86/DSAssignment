115d6eb26219a0edda329e59634bf3a19689a719
c21adano <102599332+c21adano@users.noreply.github.com>
2023-09-18 13:22:15 +0200

    improved the ability to add new students
    improved the ability to add new student and it will only accept names that are not in the list

--- C#/DSAssignmentC#/ConsoleApp1/QueueServer.cs
+++ C#/DSAssignmentC#/ConsoleApp1/QueueServer.cs
@@ -17,35 +17,37 @@ namespace QueueServerNameSpace{
             queueList.Add(2, "Two");
             queueList.Add(3, "Three");
             queueList.Add(1, "One");
-            while (true)
+            foreach (var kvp in queueList)
             {
-
-
+                Console.WriteLine("ticket = {0}, name = {1}", kvp.Key, kvp.Value);
+            }
+            Console.WriteLine("-----------------------------------");
+           
                 Thread addToListThread = new Thread(new ThreadStart(addToList));
                 addToListThread.Start();
-                Thread removeFromListThread = new Thread(new ThreadStart(removeFromList));
-                removeFromListThread.Start();
                 using (var pub = new PublisherSocket())
                 {
                     pub.Bind("tcp://*:5555");
-
-
                     while (true)
                     {
+
+
                         static string MyDictionaryToJson(IDictionary<int, string> dict)
                         {
+
                             var x = dict.Select(d =>
                                 string.Format("\"ticket\": {0}, \"name\": \"{1}\"", d.Key, string.Join(",", d.Value)));
                             return "[{" + string.Join("},{", x) + "}]";
                         }
                         string js2 = MyDictionaryToJson(queueList);
+                        pub.SendMoreFrame("queue").SendFrame(js2);// Message
+                        Console.WriteLine("lmao");
 
-                        
-                        pub.SendMoreFrame("queue").SendFrame(js2); // Message
-                        Console.WriteLine(js2);
                     }
-                }
-            }
+
+                }  
+            
+            
         }
 
         public static void addToList()
@@ -55,29 +57,42 @@ namespace QueueServerNameSpace{
                 server.Bind("tcp://*:5556");
                 while (true)
                 {
+                    Thread removeFromListThread = new Thread(removeFromList);
                     string msg = server.ReceiveFrameString();
                     Console.WriteLine("From Client: {0}", msg);
                     server.SendFrame("{}");
 
                     dynamic jsonObj = JsonConvert.DeserializeObject(msg);
                     string studentName = jsonObj.name;
-
-                    Console.WriteLine(studentName);
-
-                    var maxKey = queueList.Keys.Max();
-                    int newMaxKey = maxKey + 1;
-                    queueList.Add(newMaxKey, studentName);
+                    if (!(queueList.ContainsValue(studentName)))
+                    {
+                        var maxKey = queueList.Keys.Max();
+                        int newMaxKey = maxKey + 1;
+                        queueList.Add(newMaxKey, studentName);
+                        removeFromListThread.Start(newMaxKey);
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine(studentName + " was already in the list");
+                    }
                 }
             }
         }
 
-        public static void removeFromList()
+        public static void removeFromList(object keyValue)
         {
             //heartbeat listener, plan is to start a thread/algorithm that checks every index for a match from heartbeat.
             //if no match is found after 4 seconds delete the index
 
             //removes object with key 1 from ditonary
-            queueList.Remove(1);
+            //queueList.Remove(1);
+
+            //foreach(KeyValuePair<int, string> entry in  queueList)
+            //{
+            //    Console.WriteLine(entry.Value);
+            // }
+            Console.WriteLine("new key added to list:" + keyValue);
         }
     }
 }
